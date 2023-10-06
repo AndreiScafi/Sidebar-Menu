6b177cf3adaa2105b472709aa828aeb225a8038c
Andrei Scafi <andreiscafi@hotmail.com>
2023-10-06 09:47:44 -0300

    Adding functionality to Hamburger Menu with Javascript.

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Hamburger Menu */
+const sidebar = document.querySelector('.sidebar');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('click', () => {
+    sidebar.classList.toggle('change');
+})
+
+/* End of Hamburger Menu */
+
